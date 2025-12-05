ab8c6aa157565aa2df76a92b2362a6b4b6facd2e
shahjaynish <shahjaynnish1225@gmail.com>
2025-12-05 13:06:15 +0530

    Commit with release in module libraray gradle

--- swipeviewpager/build.gradle.kts
+++ swipeviewpager/build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     alias(libs.plugins.android.library)
     alias(libs.plugins.kotlin.android)
+    id("maven-publish")
 }
 
 android {
@@ -41,4 +42,18 @@ dependencies {
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
+}
+
+afterEvaluate {
+    publishing {
+        publications {
+            register<MavenPublication>("release") {
+                from(components["release"])
+
+                groupId = "com.github.Excelsior-Technologies-Community"    // replace
+                artifactId = "swipeviewpager"             // artifact name to publish
+                version = "stages"                        // when using branch, version can be the branch name
+            }
+        }
+    }
 }
